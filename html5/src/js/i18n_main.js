bc9d1c4d79619ac900a4e3d39320fba04b44945d
Oliver Merkel <Merkel.Oliver@web.de>
2014-03-14 22:46:07 +0100

    Consolidate Code Breaker 4.6 and Code Breaker 5.10 in one HMI. Introduce more detailed options menu.

--- html5/src/js/i18n_main.js
+++ html5/src/js/i18n_main.js
@@ -1,8 +1,11 @@
 
 function I18n() {
   this.data = {
+    'menu' : {
+      'de-de' : 'Men&uuml;',
+      'en-us' : 'Menu'},
     'selectmenuentry' : {
-      'de-de' : 'W&auml;hle Men&uuml;eintrag',
+      'de-de' : 'Men&uuml;eintrag w&auml;hlen',
       'en-us' : 'Select menu entry'},
     'checking' : {
       'de-de' : 'Pr&uuml;fen',
@@ -13,18 +16,33 @@ function I18n() {
     'license' : {
       'de-de' : 'Lizensierung',
       'en-us' : 'License' },
-    'language' : {
-      'de-de' : 'Sprache',
-      'en-us' : 'Idiom' },
     'testing' : {
       'de-de' : 'Testen',
       'en-us' : 'Testing' },
     'environment' : {
       'de-de' : 'W&auml;hle eine Testumgebung',
       'en-us' : 'Choose a test environment' },
+    'menu-options' : {
+      'de-de' : 'Einstellungen',
+      'en-us' : 'Settings' },
+    'options' : {
+      'de-de' : 'Einstellungen',
+      'en-us' : 'Settings' },
+    'about' : {
+      'de-de' : '&Uuml;ber',
+      'en-us' : 'About' },
     'languageselection' : {
       'de-de' : 'Sprachauswahl',
       'en-us' : 'Language selection' },
+    'codesize' : {
+      'de-de' : 'Gr&ouml;&szlig;e der Codes',
+      'en-us' : 'Code size' },
+    'digits4letters6' : {
+      'de-de' : '4 Stellen mit 6 Werten',
+      'en-us' : '4 digits w/ 6 letters' },
+    'digits5letters10' : {
+      'de-de' : '5 Stellen mit 10 Werten',
+      'en-us' : '5 digits w/ 10 letters' },
   };
 }
 
@@ -37,5 +55,5 @@ I18n.prototype.setLanguage = function(language) {
 translator = new I18n();
 translator.setLanguage('en-us');
 
-$('#de-de').click(function() { translator.setLanguage('de-de'); });
-$('#en-usuk').click(function() { translator.setLanguage('en-us'); });
+$('#radio-de-de').click(function() { translator.setLanguage('de-de'); });
+$('#radio-en-usuk').click(function() { translator.setLanguage('en-us'); });
--- html5/src/js/main.js
+++ html5/src/js/main.js
@@ -1,65 +1,122 @@
 
-var secret = new Secret((new SecretGenerator(4, 6)).secret);
+var secret, digits, values;
 
-var empty = '-';
-var guess = noGuess = empty + empty + empty + empty;
+var EMPTY = '-',
+    NOGUESS = EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY;
 
 function type(key) {
-  guess = guess.replace(new RegExp(empty), key);
+  guess = guess.replace(new RegExp(EMPTY), key);
   render();
 }
 
 function render() {
   var lowRes = (window.innerWidth < 350) || (window.innerHeight< 350);
   for(var digit=0; digit<guess.length; digit++) {
-    $('#digit' + digit).html(empty == guess[digit] ? '&nbsp;': guess[digit]);
+    $('#digit' + digit).html(EMPTY == guess[digit] ? '&nbsp;': guess[digit]);
   }
-  if( -1 == guess.indexOf(empty) ) {
+  if( -1 == guess.indexOf(EMPTY) ) {
     if(lowRes) {
       $('#code_keys').hide(1000);
     }
     $('#check').show();
-  }
-  else {
+  } else {
     $('#code_keys').show(1000);
     $('#check').hide();
   }
-  if( noGuess == guess ) {
+  if( NOGUESS.slice(0,digits) == guess ) {
     if(lowRes) {
       $('#current_guess').hide(1000);
     }
-  }
-  else {
+  } else {
     $('#current_guess').show(1000);
   }
 }
 
 function renderLog() {
   var result = secret.compare(guess);
-  var logEntry = '<li class="ui-li ui-li-static ui-btn-up-c ui-li-last">';
-  logEntry += '<p class="ui-li-aside ui-li-desc">';
-  logEntry += 'perfect <strong>' + result[0] + '</strong><br />';
+  var logEntry = '<li class="ui-li ui-li-static ui-btn-up-c ui-li-last" style="padding-top: 0; padding-bottom: 0;">';
+  logEntry += '<p class="ui-li-aside ui-li-desc" style="top: 2px;">';
+  logEntry += 'perfect <strong>' + result[0] + '</strong> / ';
   logEntry += 'imperfect <strong>' + result[1] + '</strong></p>';
-  logEntry += '<h3 class="ui-li-heading">' + guess + '</h3>';
+  logEntry += '<h3 class="ui-li-heading" style="margin: 0;">' + guess + '</h3>';
   logEntry += '</li>';
   $('#log').prepend(logEntry);
 }
 
 function check() {
-  if( -1 == guess.indexOf(empty) ) {
+  if( -1 == guess.indexOf(EMPTY) ) {
     renderLog();
   }
 }
 
-$('#guess_digit_0').bind('click', function(){guess = guess.replace(/./, empty); render();});
-$('#guess_digit_1').bind('click', function(){guess = guess.replace(/(.)./, '$1' + empty); render();});
-$('#guess_digit_2').bind('click', function(){guess = guess.replace(/(..)./, '$1' + empty); render();});
-$('#guess_digit_3').bind('click', function(){guess = guess.replace(/(...)./, '$1' + empty); render();});
-$('#selected_a').bind('click', function(){type('a');});
-$('#selected_b').bind('click', function(){type('b');});
-$('#selected_c').bind('click', function(){type('c');});
-$('#selected_d').bind('click', function(){type('d');});
-$('#selected_e').bind('click', function(){type('e');});
-$('#selected_f').bind('click', function(){type('f');});
-$('#check').bind('click', function(){check();});
-render();
+function typeValue( e ) {
+  var id = e.target.id;
+  type(id[id.length-1]);
+}
+
+function setupValues() {
+  var group = $('#code_keys');
+  group.controlgroup('container').html('');
+  var ctrls = '';
+  for(var i=0; i<values; ++i) {
+    var chr = 'abcdefghijklmnopqrstuvwxyz'.charAt(i);
+    ctrls += "<a href='#' data-role='button' data-icon='plus' " +
+      "data-iconpos='top' class='mybutton' id='value_" + chr + "'>" +
+      chr + "</a>";
+  }
+  group.controlgroup('container').append(ctrls);
+  group.enhanceWithin().controlgroup('refresh');
+  for(var i=0; i<values; ++i) {
+    var chr = 'abcdefghijklmnopqrstuvwxyz'.charAt(i);
+    $('#value_' + chr).bind('click', typeValue);
+  }
+}
+
+function removeDigit( e ) {
+  var id = e.target.id;
+  var position = parseInt(id[id.length-1]);
+  guess[position] = EMPTY;
+  guess = guess.slice(0, position) + EMPTY + guess.slice(position+1);
+  render();
+}
+
+function setupDigits() {
+  var group = $('#current_guess');
+  group.controlgroup('container').html('');
+  var ctrls = '';
+  for(var i=0; i<digits; ++i) {
+    ctrls += "<a id='guess_digit_" + i + "' data-role='button' " +
+      "data-icon='delete' data-iconpos='top' class='mybutton'>" +
+      "<span id='digit" + i + "'>&nbsp;</span></a>";
+  }
+  group.controlgroup('container').append(ctrls);
+  group.enhanceWithin().controlgroup('refresh');
+  for(var i=0; i<digits; ++i) {
+    $('#guess_digit_' + i).bind('click', removeDigit);
+  }
+}
+
+function newGame() {
+  if ( $('#radio-5digits').is(':checked') ) {
+    digits = 5;
+    values = 10;
+  } else {
+    digits = 4;
+    values = 6;
+  }
+  secret = new Secret((new SecretGenerator(digits, values)).secret);
+  guess = NOGUESS.slice(0,digits);
+  $('#log').html('');
+  setupValues();
+  setupDigits();
+  render();
+}
+
+function initialize() {
+  $('#check').bind('click', check);
+  $('#command-new').bind('click', newGame);
+  newGame();
+  render();
+}
+
+$( initialize );
