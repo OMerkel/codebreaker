8721babd088745d2cd5ebc50868c6ae78a3e38cc
Oliver Merkel <Merkel.Oliver@web.de>
2014-03-26 19:47:48 +0100

    i18n : J'ai ajouté la langue française.

--- html5/src/js/i18n_main.js
+++ html5/src/js/i18n_main.js
@@ -3,52 +3,84 @@ function I18n() {
   this.data = {
     'menu' : {
       'de-de' : 'Men&uuml;',
-      'en-us' : 'Menu'},
+      'en-us' : 'Menu',
+      'fr-fr' : 'Menu',
+    },
     'selectmenuentry' : {
       'de-de' : 'Men&uuml;eintrag w&auml;hlen',
-      'en-us' : 'Select menu entry'},
+      'en-us' : 'Select menu entry',
+      'fr-fr' : 'S&eacute;lectionnez une option de menu',
+    },
     'checking' : {
       'de-de' : 'Pr&uuml;fen',
-      'en-us' : 'Check' },
+      'en-us' : 'Check',
+      'fr-fr' : 'Tester',
+    },
     'new' : {
       'de-de' : 'Neu',
-      'en-us' : 'New' },
+      'en-us' : 'New',
+      'fr-fr' : 'Nouveau',
+    },
     'license' : {
       'de-de' : 'Lizensierung',
-      'en-us' : 'License' },
+      'en-us' : 'License',
+      'fr-fr' : 'Licence',
+    },
     'testing' : {
       'de-de' : 'Testen',
-      'en-us' : 'Testing' },
+      'en-us' : 'Testing',
+      'fr-fr' : 'Tester',
+    },
     'environment' : {
       'de-de' : 'W&auml;hle eine Testumgebung',
-      'en-us' : 'Choose a test environment' },
+      'en-us' : 'Choose a test environment',
+      'fr-fr' : 'Choisissez un environnement de test',
+    },
     'menu-options' : {
       'de-de' : 'Einstellungen',
-      'en-us' : 'Settings' },
+      'en-us' : 'Settings',
+      'fr-fr' : 'Param&eacute;trage',
+    },
     'options' : {
       'de-de' : 'Einstellungen',
-      'en-us' : 'Settings' },
+      'en-us' : 'Settings',
+      'fr-fr' : 'Param&eacute;trage',
+    },
     'about' : {
       'de-de' : '&Uuml;ber',
-      'en-us' : 'About' },
+      'en-us' : 'About',
+      'fr-fr' : 'A propos',
+    },
     'about-title' : {
       'de-de' : '&Uuml;ber Code Breaker',
-      'en-us' : 'About Code Breaker' },
+      'en-us' : 'About Code Breaker',
+      'fr-fr' : 'A propos Code Breaker',
+    },
     'legal-title' : {
       'de-de' : 'Rechtliches',
-      'en-us' : 'Legal' },
+      'en-us' : 'Legal',
+      'fr-fr' : 'Juridique',
+    },
     'languageselection' : {
       'de-de' : 'Sprachauswahl',
-      'en-us' : 'Language selection' },
+      'en-us' : 'Language selection',
+      'fr-fr' : 'Choisir de langue',
+    },
     'codesize' : {
       'de-de' : 'Gr&ouml;&szlig;e der Codes',
-      'en-us' : 'Code size' },
+      'en-us' : 'Code size',
+      'fr-fr' : 'Taille du code',
+    },
     'digits4letters6' : {
       'de-de' : '4 Stellen mit 6 Werten',
-      'en-us' : '4 digits w/ 6 letters' },
+      'en-us' : '4 digits w/ 6 letters',
+      'fr-fr' : '4 chiffres de 6 valeurs',
+    },
     'digits5letters10' : {
       'de-de' : '5 Stellen mit 10 Werten',
-      'en-us' : '5 digits w/ 10 letters' },
+      'en-us' : '5 digits w/ 10 letters',
+      'fr-fr' : '5 chiffres de 10 valeurs',
+    },
     'about_text' : {
       'de-de' :
         "<p><b>Code Breaker</b> ist eine Abwandlung " +
@@ -145,7 +177,55 @@ function I18n() {
             "imperfect ones is prioritized. Previous " +
             "check results are kept visible " +
             "in a log to use for analysis.</li>" +
-        "</ul>" },
+        "</ul>",
+      'fr-fr' :
+        "<p><b>Code Breaker</b> is derived from a " +
+          "game called <i>Bulls and Cows</i> of unknown " +
+          "origin. It's game principle is public domain. " +
+          "There are commercial versions of <i>Bulls and " +
+          "Cows</i> from other parties using different " + 
+          "names or trademarks being published and " +
+          "being available as well. <i>Code Breaker</i> " +
+          "is MIT licensed and just uses the public " +
+          "domain principle of the game <i>Bulls and " +
+          "Cows</i>. It comes bundled with unmodified " +
+          "third party software libraries using free " +
+          "software licensing.</p>" +
+        "<h3>Rules</h3>" +
+        "<p>The rules of <i>Code Breaker</i> are as follows.</p>" +
+        "<ul>" +
+          "<li>Iteratively a guess is " +
+            "compared towards a secret code with " +
+            "fixed length of digits and selected " +
+            "amount of possible values per digit " +
+            "(as configured in <i>options</i> menu).</li>" +
+          "<li>Objective is to match the secret " +
+            "code perfectly.</li>" +
+          "<li>On each guess a result indicates the " +
+            "amount of perfect matches and imperfect " +
+            "matches represented as numerical values." +
+            "<ul>" +
+              "<li>Perfect matches indicate that a " +
+                "digit value is included in the secret " +
+                "and is placed at correct position " +
+                "within the guess.</li>" +
+              "<li>Imperfect matches indicate that " +
+                "a digit value is included in the secret " +
+                "but the position is still incorrect.</li>" +
+            "</ul>" +
+          "</li>" +
+          "<li>Thus the secret code is found if " +
+            "all digits are perfect matches and no " +
+            "imperfect match are indicated on a check.</li>" +
+          "<li>Same values may occur multiple " +
+            "times. No digit of the guess will count " +
+            "multiple times in the indicated result. " +
+            "Indicating perfect matches over " +
+            "imperfect ones is prioritized. Previous " +
+            "check results are kept visible " +
+            "in a log to use for analysis.</li>" +
+        "</ul>",
+    },
   };
 }
 
@@ -160,3 +240,4 @@ translator.setLanguage('en-us');
 
 $('#radio-de-de').click(function() { translator.setLanguage('de-de'); });
 $('#radio-en-usuk').click(function() { translator.setLanguage('en-us'); });
+$('#radio-fr-fr').click(function() { translator.setLanguage('fr-fr'); });
