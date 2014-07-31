8c033bc979058e0c96d873576ca536fb61849f24
Oliver Merkel <Merkel.Oliver@web.de>
2014-07-31 20:37:44 +0200

    Translation into Spanish of about text and rules. Allow iOS fullscreen and define icon when installed on iOS Home Screen if using Add to Home Screen in Safari Browser.

--- html5/src/js/i18n_main.js
+++ html5/src/js/i18n_main.js
@@ -216,51 +216,58 @@ function I18n() {
       'digits4letters6' : '4 d&iacute;gitos con 6 valores',
       'digits5letters10' : '5 d&iacute;gitos con 10 valores',
       'about_text' :
-        "<p><b>Code Breaker</b> is derived from a " +
-          "game called <i>Bulls and Cows</i> of unknown " +
-          "origin. It's game principle is public domain. " +
-          "There are commercial versions of <i>Bulls and " +
-          "Cows</i> from other parties using different " + 
-          "names or trademarks being published and " +
-          "being available as well. <i>Code Breaker</i> " +
-          "is MIT licensed and just uses the public " +
-          "domain principle of the game <i>Bulls and " +
-          "Cows</i>. It comes bundled with unmodified " +
-          "third party software libraries using free " +
-          "software licensing.</p>" +
-        "<h3>Rules</h3>" +
-        "<p>The rules of <i>Code Breaker</i> are as follows.</p>" +
+        // Spanish translation of about_text done
+        // in collaboration of Oliver Merkel and Johannes Brunner
+        "<p><b>Code Breaker</b> es una variaci&oacute;n " +
+          "de un juego de origen desconocido llamado " +
+          "<i>toros y vacas</i> (en alem&aacute;n " +
+          "<i>Bullen und K&uuml;he</i>). El principio del " +
+          "juego es bajo el dominio p&uacute;blico. " +
+          "Tambi&eacute;n existen versiones comerciales " +
+          "publicados y disponibles de <i>toros y vacas</i> " +
+          "de otros fabricantes bajo diferentes nombres " +
+          "y marcas comerciales. <i>Code Breaker</i> es " +
+          "licenciado por la MIT y utiliza el principio " +
+          "de <i>toros y vacas</i> (en alem&aacute;n " +
+          "<i>Bullen und K&uuml;he</i>) de dominio " +
+          "p&uacute;blico. Se incluyen las bibliotecas " +
+          "de software sin modificaciones por parte de " +
+          "terceros que est&aacute;n bajo licencias de " +
+          "software libre.</p>" +
+        "<h3>Reglas</h3>" +
+        "<p>Estas son las reglas de <i>Code Breaker</i> en detalle.</p>" +
         "<ul>" +
-          "<li>Iteratively a guess is " +
-            "compared towards a secret code with " +
-            "fixed length of digits and selected " +
-            "amount of possible values per digit " +
-            "(as configured in <i>options</i> menu).</li>" +
-          "<li>Objective is to match the secret " +
-            "code perfectly.</li>" +
-          "<li>On each guess a result indicates the " +
-            "amount of perfect matches and imperfect " +
-            "matches represented as numerical values." +
+          "<li>Se compara los repetidos intentos con " +
+            "un c&oacute;digo secreto con una cantidad " +
+            "fija de d&iacute;gitos y una cantidad determinada " +
+            "de valores por d&iacute;gito (como seleccionado " +
+            "en el men&uacute; <i>Par&aacute;metros</i>).</li>" +
+          "<li>El objetivo es lograr una combinaci&oacute;n " +
+            "perfecta que coincide con el c&oacute;digo secreto.</li>" +
+          "<li>Despu&eacute;s de cada intento el resultado " +
+            "consta de la cantidad de d&iacute;gitos con coincidencia " +
+            "perfecta y con coincidencia imperfecta." +
             "<ul>" +
-              "<li>Perfect matches indicate that a " +
-                "digit value is included in the secret " +
-                "and is placed at correct position " +
-                "within the guess.</li>" +
-              "<li>Imperfect matches indicate that " +
-                "a digit value is included in the secret " +
-                "but the position is still incorrect.</li>" +
+              "<li>Coincidencia perfecta significa, " +
+                "que un d&iacute;gito est&aacute; en el c&oacute;digo " +
+                "secreto y tambi&eacute;n esta en la " +
+                "posici&oacute;n correcta.</li>" +
+              "<li>Coincidencia imperfecta significa, que " +
+                "un d&iacute;gito est&aacute; en el c&oacute;digo " +
+                "secreto pero la posici&oacute;n todav&iacute;a " +
+                "no es correcta.</li>" +
             "</ul>" +
           "</li>" +
-          "<li>Thus the secret code is found if " +
-            "all digits are perfect matches and no " +
-            "imperfect match are indicated on a check.</li>" +
-          "<li>Same values may occur multiple " +
-            "times. No digit of the guess will count " +
-            "multiple times in the indicated result. " +
-            "Indicating perfect matches over " +
-            "imperfect ones is prioritized. Previous " +
-            "check results are kept visible " +
-            "in a log to use for analysis.</li>" +
+          "<li>Por lo tanto, se considera el c&oacute;digo " +
+            "secreto descubierto si todos d&iacute;gitos coinciden " +
+            "perfectamente y no hay coincidencias imperfectas.</li>" +
+          "<li>Los mismos valores pueden aparecer m&aacute;s " +
+            "de una vez. En el resultado mostrado, los d&iacute;gitos " +
+            "no son comprobadas mas de una vez. La " +
+            "indicaci&oacute;n de coincidencias perfectas tiene " +
+            "prioridad sobre coincidencias imperfectas. " +
+            "Resultados anteriores permanecen visibles en " +
+            "una lista para el an&aacute;lisis.</li>" +
         "</ul>",
     },
     'pt-pt' : {
